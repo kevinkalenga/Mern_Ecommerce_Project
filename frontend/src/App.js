fe2f1654095d327c85f7d266a42333d08d988694
kevin kalenga <kevinkalenga10@gmail.com>
2025-01-28 14:15:29 +0100

    Create routes for publicly available pages list of products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import CartPage from './pages/CartPage'
+import ProductDetailsPage from './pages/ProductDetailsPage'
+import ProductListPage from './pages/ProductListPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 const App = () => {
   return (
     <BrowserRouter>
        <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/product-list" element={<ProductListPage />} />
+          <Route path="/product-details" element={<ProductDetailsPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
        </Routes> 
     </BrowserRouter>
     
