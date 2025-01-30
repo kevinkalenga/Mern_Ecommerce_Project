2bf434e8b2743ad3926f7d12e7edf4e04993f367
kevin kalenga <kevinkalenga10@gmail.com>
2025-01-30 13:04:41 +0100

    Style footer component

--- frontend/src/components/FooterComponent.js
+++ frontend/src/components/FooterComponent.js
@@ -1,7 +1,16 @@
-
+import {Container, Row, Col} from 'react-bootstrap'
 const FooterComponent = () => {
   return (
-    <div>FooterComponent</div>
+   <footer>
+    <Container fluid>
+        <Row className='mt-5'>
+           <Col className='bg-dark text-white text-center py-5'>
+             Copyright &copy; Best Online Shop
+           </Col>
+        </Row>
+        
+    </Container>
+  </footer>
   )
 }
 
