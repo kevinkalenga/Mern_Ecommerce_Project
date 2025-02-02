48522e4c50b9bc5454b4dad69cc82c21c0986d84
kevin kalenga <kevinkalenga10@gmail.com>
2025-02-03 00:23:48 +0100

    Chat Popup - Blue Circle With Icons

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -1,6 +1,15 @@
+
+import "../../chats.css"
 const UserChatComponent = () => {
   return (
-    <div>UserChatComponent</div>
+    <>
+      <input type="checkbox" id="check" />
+      <label className="chat-btn" htmlFor="check">
+         <i className="bi bi-chat-dots comment"></i>
+         <i class="bi bi-x-circle close"></i>
+      </label>
+      
+    </>
   )
 }
 
