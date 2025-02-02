97705bc82dacfe0bf09e4b2e439606ec005042c9
kevin kalenga <kevinkalenga10@gmail.com>
2025-02-03 00:51:35 +0100

    Chat Popup - Message Field and Submit Button

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -13,6 +13,21 @@ const UserChatComponent = () => {
            <h6>Lest's Chat - Online</h6>
 
         </div>
+        <div className="chat-form">
+           <div className="cht-msg">
+              <p>Chat history</p>
+           </div>
+           <textarea 
+              id="clientChatMsg"
+              className="form-control"
+              placeholder="Your Text Message"
+            >
+
+           </textarea>
+           <button className="btn btn-success btn-block">
+             Submit
+           </button>
+        </div>
 
       </div>
     </>
